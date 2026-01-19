3a82e9bcfe614f624b01949076aedf89dc7aaa6c
Ayush Kumar Singh <iamayushsingh02@gmail.com>
2026-01-20 02:19:10 +0530

    day 3 done

---
+++ dsa/nqt/day3.py
@@ -0,0 +1,57 @@
+#1
+# n=int(input())
+# for i in range(1,n+1):
+#   if i%15==0:
+#     print('FizzBuzz')
+#   elif i%3==0:
+#     print('Fizz')
+#   elif i%5==0:
+#     print('Buzz')
+#   else:
+#     print(i)
+
+#2
+# n=int(input('Input: '))
+# count2=count3=0
+# if n<=0:
+#   print(0)
+# else:
+#   for i in range(1,n+1):
+#     if i%6==0:
+#       continue
+#     elif i%2==0:
+#       count2+=1
+#     elif i%3==0:
+#       count3+=1
+#   print("Output:",count3+count2)
+
+#3
+# n=int(input(''))
+# count=0
+# for i in range(1,n+1):
+#   if i%6!=0 and i%4==0:
+#     count+=i
+# print(count)
+
+#4
+# n=int(input())
+# ans=-1
+# if n<=0:
+#   print(-1)
+# else:
+#   for i in range(1,n+1):
+#     if i%77==0:
+#       ans=i
+#       break
+# print(ans)
+
+#5
+n=int(input())
+if n<=0:
+  print(0)
+else:
+  for i in range(1,n+1):
+    if i%7==0:
+      break
+    elif i%5==0:
+      print(i)      
