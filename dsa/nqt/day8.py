17f871ebc32714ae9a6202156017f93d76b02914
Ayush Kumar Singh <iamayushsingh02@gmail.com>
2026-01-23 00:59:57 +0530

    day 8 done

---
+++ dsa/nqt/day8.py
@@ -0,0 +1,46 @@
+#1
+# num=list(map(int,input().split()))
+# n=len(num)
+# ans=-1
+# avgg=sum(num)/n
+# for i in range(n):
+#   if num[i]%2!=0 and num[i]>avgg:
+#     temp=abs(num[i])
+#     if temp==0:
+#       count=1
+#     else:
+#       count=0
+#       while temp>0:
+#         temp//=10
+#         count+=1
+#     if count%2==0:
+#       ans=num[i]
+#       break
+# print(ans)
+
+#2
+# s=input()
+# ans=-1
+# freq={}
+# for ch in s:
+#   freq[ch]=freq.get(ch,0)+1
+# for ch in range(1,len(s)-1):
+#   if s[ch].isalpha() and freq[s[ch]]==2 and (ord(s[ch-1])+ord(s[ch+1]))%2==0:     
+#       ans=s[ch]
+#       break
+# print(ans)
+
+#3
+num=list(map(int,input().split()))
+n=len(num)
+left=0
+ans=-1
+total=sum(num)
+for i in range(n):
+  right=total-left-num[i]
+  if left==right:
+    ans=num[i]
+    break
+  else:
+    left+=num[i]
+print(ans)
